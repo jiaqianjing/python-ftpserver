7362ce43cd07a13b4dc22310f96f2c23f111bc31
King <jiaqianjing@gmail.com>
2021-01-25 10:17:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-nohup python ftpserver.py > ftpserver.log 2>&1 &
-echo $! > ftpserver.pid
+# 依赖
+pip install pyftpdlib
+
+# 启动 server
+share_dir="/home/jiaqianjing/workspace"
+user="jiaqianjing"
+passwd="1234"
+nohup python -m pyftpdlib -i 0.0.0.0 -p 2122 -w -d $share_dir -u $user -P $passwd >run.log 2>&1 &
+echo $! > run.pid
