1e61b5ccda061e1da478671e95422794db474385
King <jiaqianjing@gmail.com>
2021-01-30 15:18:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 pip install pyftpdlib
 
 # 启动 server
-share_dir="/home/jiaqianjing/workspace"
+share_dir=$PWD
 user="jiaqianjing"
 passwd="1234"
 nohup python -m pyftpdlib -i 0.0.0.0 -p 2122 -w -d $share_dir -u $user -P $passwd >run.log 2>&1 &
